1343ad5cca215c251415d7b8c73a0827385aaf22
Storrtor <storrtor@gmail.com>
2021-05-19 19:52:19 +0300

    удалили ветки

--- src/Try1/Try1.java
+++
@@ -1,7 +0,0 @@
-package Try1;
-
-public class Try1 {
-    public static void main(String[] args) {
-        System.out.println("PRIVET");
-    }
-}
--- src/Try4.java
+++
@@ -1,5 +0,0 @@
-public class Try4 {
-    public static void main(String[] args) {
-        System.out.println("ggdfgdfgdgd");
-    }
-}
